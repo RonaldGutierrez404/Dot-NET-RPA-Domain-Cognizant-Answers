6a58ecaa5bb0bebf1883384fc56f335fed1eb683
Paria <2321913@cognizant.com>
2024-01-29 11:00:47 +0530

    Directory Name Changed

---
+++ Dotnet Integrated Capability Test (ICT)/DBHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace ADO_Net_App1
+{
+    public class DBHandler
+    {
+        public OracleConnection GetConnection()
+        {
+            // Create a new OracleConnection object with the connection string from DBConnection
+            OracleConnection conn = new OracleConnection(DBConnection.connStr);
+
+            // Return the OracleConnection object
+            return conn;
+        }
+    }
+}
